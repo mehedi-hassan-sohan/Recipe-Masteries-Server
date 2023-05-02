7a0147a630b4de7ed39a0e3e85904f13d26bd90d
mehedi-hassan-sohan <mehedihassansohan80@gmail.com>
2023-05-02 18:37:41 +0600

    json data created

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 const express = require('express')
 const app = express()
-const port = 5000
-const cors = ('cors') 
-app.use(cors)
+const port = process.env.PORT || 5000
+const cors =require('cors') 
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Chafe recipe server  in running')
