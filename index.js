30f99e9856f7837d3ee5a66f5bf49c6414bb9cc9
mehedi-hassan-sohan <mehedihassansohan80@gmail.com>
2023-05-03 16:42:33 +0600

    data make for chef

--- index.js
+++ index.js
@@ -3,10 +3,21 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 const cors =require('cors') 
+const home = require('./Data/Chaf.json')
 app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Chafe recipe server  in running')
+})
+app.get('/home', (req, res) => {
+  
+  res.send(home)
+}) 
+ 
+app.get('/home/:id',(req,res)=>{
+  const id = req.params.id
+  const selectedInfo = home.find(n=>n.id === id) 
+  res.send(selectedInfo)
 })
 
 app.listen(port, () => {
